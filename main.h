0b111411a6fe14c29d501b77e471af5efc0e2cf7
muzaffar <muzaffar@junggu-da.com>
2022-11-10 10:30:15 +0900

    초기 커밋.

---
+++ main.h
@@ -0,0 +1,31 @@
+#pragma once
+// wxWidgets "Hello World" Program
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+
+enum
+{
+    ID_Hello = 1
+};
